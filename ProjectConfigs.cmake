4e0a5c18d6cfbbd51ba955061c873f5bea74d004
Curtis Cao <caohengvs@163.com>
2026-01-14 10:50:45 +0800

    feat: 项目初始化
    -
    -
    -

---
+++ ProjectConfigs.cmake
@@ -0,0 +1,4 @@
+set(Qt6_ROOT "D:/QtDevelop/qt6.10/6.10.1/msvc2022_64")
+set(3rd_PARTY_ROOT "D:/repos/3rdParty")
+set(CMAKE_TOOLCHAIN_FILE "D:/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "vcpkg toolchain file")
+set(VCPKG_TARGET_TRIPLET "x64-windows" CACHE STRING "")
